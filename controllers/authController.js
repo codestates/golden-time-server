f09bee71d8c1c2273cf3d6a4549ea79670d1e5fd
이재홍 <fleuve1008@gmail.com>
2020-12-23 19:44:25 +0900

    Add local login and jwt token auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
-const bcryto = require('bcrypt');
 
 exports.signup = async (req, res) => {
   const { email, nick, password } = req.body;
   if (!email || !nick || !password) return res.status(400).send('error');
-  const hash = await bcryto.hash(password, 12);
+  const hash = await bcrypt.hash(password, 12);
   const exUser = await User.findOrCreate({
     where: { email },
-    defaults: { email, nick, password: hash },
+    defaults: { nick, password: hash },
   });
   const [user, created] = exUser;
   if (created) {
@@ -17,14 +20,35 @@ exports.signup = async (req, res) => {
   }
 };
 
-exports.signin = (req, res) => {};
+exports.signin = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: info.message, redirect_url: '/login' });
+    }
+    return req.login(user, (err) => {
+      if (err) return next(err);
+      const token = jwt.sign(
+        { id: user.id, nick: user.nick, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' },
+      );
+      res
+        .status(200)
+        .cookie('access_token', token)
+        .json({ access_token: token, redirect_url: '/' });
+    });
+  })(req, res, next);
+};
 
 exports.signout = (req, res) => {};
 
 exports.userInfo = (req, res) => {};
 
-exports.google = (req, res) => {};
+// exports.google = (req, res) => {};
 
-exports.kakao = (req, res) => {};
+// exports.kakao = (req, res) => {};
 
-exports.modify = (req, res) => {};
+// exports.modify = (req, res) => {};
--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const authController = require('../controllers');
 const router = express.Router();
 
 router.post('/signup', authController.auth.signup);
+router.post('/signin', authController.auth.signin);
 
 module.exports = router;
