dbc23191b474e754b7175d55d24d4a136798165b
CHO-cmd <wjdqls9362@gmail.com>
2020-12-26 22:22:31 +0900

    completed addComment

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const commentsRouter = require('../controllers');
+const commentsController = require('../controllers');
+const passport = require('passport');
 
 const router = express.Router();
+
+router.post(
+  '/addcomment',     
+  passport.authenticate('jwt', { session: false }),
+  commentsController.comment.addComment,
+);
 
 module.exports = router;
