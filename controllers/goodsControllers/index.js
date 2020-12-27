e7ebf57414c633a8b45b48132fa473f4214b8ce9
이재홍 <fleuve1008@gmail.com>
2020-12-28 07:51:05 +0900

    Add goods delete

--- controllers/goodsControllers/index.js
+++ controllers/goodsControllers/index.js
@@ -1,3 +1,4 @@
 exports.addGoods = require('./addGoods');
 exports.modifiedGoods = require('./modifiedGoods');
 exports.goodsList = require('./goodsList');
+exports.deleteGoods = require('./deleteGoods');
