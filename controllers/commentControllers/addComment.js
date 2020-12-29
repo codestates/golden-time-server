77e5188d3aeaca8b9d2245ca22700e96c58efed1
CHO-cmd <wjdqls9362@gmail.com>
2020-12-29 15:32:52 +0900

    applied try catch in addComment.js

--- controllers/commentControllers/addComment.js
+++ controllers/commentControllers/addComment.js
@@ -1,24 +1,28 @@
 const { Comment } = require('../../models');
 
-module.exports = async (req, res) => {
-  const { goodsId, commentMessage } = req.body;
-  const { id, nick, profileImage } = req.user;
+module.exports = async (req, res, next) => {
+  try {
+    const { goodsId, commentMessage } = req.body;
+    const { id, nick, profileImage } = req.user;
 
-  let comment = await Comment.create({
-    userId: id,
-    commentMessage,
-    goodsId,
-  });
+    let comment = await Comment.create({
+      userId: id,
+      commentMessage,
+      goodId: goodsId,
+    });
 
-  res.status(200)
-  .json({ 
-    commentId: comment.id,
-    commentMessage: comment.commentMessage,
-    createdAt: comment.createdAt,
-    user: {
-      id,
-      nick,
-      profileImage,
-    }
-  });
+    res.status(200)
+    .json({ 
+      commentId: comment.id,
+      commentMessage: comment.commentMessage,
+      createdAt: comment.createdAt,
+      user: {
+        id,
+        nick,
+        profileImage,
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
 };
