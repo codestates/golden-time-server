4843f5af1313840973d7d94529b366fdb45eaefb
이재홍 <fleuve1008@gmail.com>
2020-12-29 10:31:07 +0900

    Fix app.js And config

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '..', 'config', 'config.js'))[env];
 const db = {};
-
+console.log(config);
 const sequelize = new Sequelize(
   config.database,
   config.username,
