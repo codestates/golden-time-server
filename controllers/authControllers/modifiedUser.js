317d4a8544d48209cb24ff5cc921deeb1f44d148
이재홍 <fleuve1008@gmail.com>
2020-12-30 14:02:07 +0900

    Fix user profile image bug and goods infomation edit bug

--- controllers/authControllers/modifiedUser.js
+++ controllers/authControllers/modifiedUser.js
@@ -1,28 +1,32 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const path = require('path');
-const { User } = require('../../models');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
+const { User } = require("../../models");
 
 module.exports = async (req, res, next) => {
   try {
     const { nick } = req.body;
     const findImage = await User.findOne({
       where: { id: req.user.id },
-      attributes: ['profileImage'],
+      attributes: ["profileImage"],
     });
 
-    if (findImage.profileImage && findImage.profileImage.includes('uploads')) {
+    if (
+      req.file &&
+      findImage.profileImage &&
+      findImage.profileImage.includes("uploads")
+    ) {
       fs.unlink(
-        path.join(__dirname, '../..', findImage.profileImage),
+        path.join(__dirname, "../..", findImage.profileImage),
         (err) => {
           if (err) throw err;
-        },
+        }
       );
     }
     await User.update(
-      { nick, profileImage: req.file.path },
-      { where: { id: req.user.id } },
+      { nick, profileImage: req.file ? req.file.path : findImage.profileImage },
+      { where: { id: req.user.id } }
     );
     const updateInfo = await User.findOne({ where: { id: req.user.id } });
     const token = jwt.sign(
@@ -34,9 +38,9 @@ module.exports = async (req, res, next) => {
         profileImage: updateInfo.profileImage,
       },
       process.env.JWT_SECRET,
-      { expiresIn: '7d' },
+      { expiresIn: "7d" }
     );
-    res.status(200).cookie('access_token', token).json({ access_token: token });
+    res.status(200).cookie("access_token", token).json({ access_token: token });
   } catch (err) {
     next(err);
   }
--- controllers/goodsControllers/deleteGoods.js
+++ controllers/goodsControllers/deleteGoods.js
@@ -1,27 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const { Goods, GoodsImage } = require('../../models');
+const fs = require("fs");
+const path = require("path");
+const { Goods, GoodsImage } = require("../../models");
 
 module.exports = async (req, res, next) => {
   try {
     const { goodsId } = req.body;
 
-    const images = GoodsImage.findAll({
+    const images = await GoodsImage.findAll({
       where: { goodId: goodsId },
-      attributes: ['imagePath'],
+      attributes: ["imagePath"],
     });
     for (let i = 0; i < images.length; i++) {
-      if (image[i].imagePath && image[i].imagePath.includes('uploads')) {
-        fs.unlink(path.join(__dirname, '../..', image[i].imagePath), (err) => {
+      if (images[i].imagePath && images[i].imagePath.includes("uploads")) {
+        fs.unlink(path.join(__dirname, "../..", images[i].imagePath), (err) => {
           if (err) throw err;
         });
       }
     }
+    await Goods.destroy({ where: { id: goodsId } });
+    await GoodsImage.destroy({
+      where: { goodId: goodsId },
+      force: true,
+    });
 
-    await Goods.destroy({ where: { id: goodsId }, truncate: true });
-    await GoodsImage.destroy({ where: { goodId: goodsId }, truncate: true });
-
-    res.status(200).json({ redirect_url: '/' });
+    res.status(200).json({ redirect_url: "/" });
   } catch (err) {
     next(err);
   }
